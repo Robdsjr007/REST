72f9e0c508827383400f4c8cb424878d4dad8552
Robson Dias De Santana Junior <robson.santana15@etec.sp.gov.br>
2023-05-26 08:48:46 -0300

    Criação do POST

--- POST/sucesso.php
+++ POST/sucesso.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de Cadastro</title>
-    
+
     <style>
-        html{
+        html {
             width: 100%;
             height: 100%;
         }
-        body{
+
+        body {
             width: 100%;
             height: 100%;
         }
-        content{
+
+        content {
             display: flex;
             justify-content: center;
             align-items: center;
             width: 100%;
             height: 100%;
         }
-        .card{
+
+        .card {
             display: flex;
             flex-direction: column;
             border: black 1px solid;
@@ -30,22 +34,14 @@
         }
     </style>
 </head>
-<body>
-<button><a href="index.html">Voltar a Página principal</a></button>
- 
-<content>
-    <div class="card">
-        <?php
-        // Ler o arquivo com os usuários cadastrados
-        $json = file_get_contents('../users.txt');
-        $usuarios = json_decode($json, true);
-        
-        // Envia o ID fornecido na URL
 
-        echo 'Olá mundo!';
-        
-        ?>
-    </div>
-</content>
+<body>
+    <button><a href="index.html">Voltar a Página principal</a></button>
+    <content>
+        <div class="card">
+            <?php echo 'hello world' ; ?>
+        </div>
+    </content>
 </body>
+
 </html>
